8d9265116e5586170e2c2473f16c95d308b5fa55
罗杰 <rmarrero@car8s5.42barcelona.com>
2025-01-31 13:35:02 +0100

    ft_atoi.c [OK], norminette [OK]

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,12 @@
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 18:38:41 by rmarrero          #+#    #+#             */
-/*   Updated: 2024/09/18 06:44:35 by rmarrero         ###   ########.fr       */
+/*   Updated: 2025/01/24 16:28:33 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+// --- ft_atoi version for converting strings to integers --- //
 int	ft_atoi(const char *nptr)
 {
 	int		result;
@@ -36,41 +37,3 @@ int	ft_atoi(const char *nptr)
 	}
 	return (result * sign);
 }
-/*
-void test_comparison(const char *str) {
-    int result_ft = ft_atoi(str);
-    int result_std = atoi(str);
-
-    printf("Input: \"%s\"\n", str);
-    printf("ft_atoi result: %d\n", result_ft);
-    printf("atoi result: %d\n", result_std);
-
-    if (result_ft == result_std) {
-        printf("PASSED: Both results are the same.\n");
-    } else {
-        printf("FAILED: Results are different.\n");
-    }
-    printf("--------\n");
-}
-
-int main() {
-    test_comparison("123");
-    test_comparison("-456");
-    test_comparison("0");
-    test_comparison("   789");
-    test_comparison("   -234");
-    test_comparison("12abc34");
-    test_comparison("abc123");
-    test_comparison("");
-    test_comparison(" ");
-    test_comparison("+");
-    test_comparison("-");
-    test_comparison("2147483648");
-    test_comparison("2147483649");
-    test_comparison("-2147483627172");
-    test_comparison("-2147483649");
-    test_comparison("----2147483648");
-    test_comparison("-+2147483649");
-
-    return 0;
-}*/
